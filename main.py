42e95d51d2fb42e58cb1fe595ac27a3a17dff73f
Tahmid Hasan <67729687+IamTahmidHasan14@users.noreply.github.com>
2023-09-25 19:37:52 +0600

    main.py

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-#SINGLE INHERITANCE
-class Animal:
+#MULTIPLE INHERITANCE
+class Classmate:
 
-  def __init__(self, name, type):
+  def __init__(self, name, cls):
     self.name = name
-    self.type = type
+    self.cls = cls
 
-  def sound(self):
-    print(f"Sound by a {self.type}")
-
-
-class Cat(Animal):
-
-  def __init__(self, name, chield):
-    super().__init__(name, type="cat")
-    self.chield = chield
-
-  def sound(self):
-    print("Meaw!")
+  def show(self):
+    print(f"My friend name is {self.name}. He reads in {self.cls}")
 
 
-c = Cat("Lusni", "Mini")
-c.sound()
-a = Animal("Lusni", "Cat")
-a.sound()
+class AreaFriend:
+
+  def __init__(self, name, area):
+    self.name = name
+    self.area = area
+
+  def show(self):
+    print(f"My friend name is {self.name}. He lives in {self.area}")
+
+
+class Friend(Classmate, AreaFriend):  #MULTIPLE INHERITANCE
+
+  def __init__(self, name, cls, area):
+    self.name = name
+    self.cls = cls
+    self.area = area
+
+
+f1 = Friend("Masum", "", "Shalbagan")
+f1.show()  #first a classmate dewa ache, ejonno classmate class call hobe
+f2 = Friend("Mahmud", "4th Year", "")
+f2.show()
+a = AreaFriend("Masum", "Shalbagan")
+a.show()
